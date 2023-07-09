ad2ed7352bdcc51ed2df0137d3afb0432d9c7d2a
raul cervantes <raulcer.rmc@gmail.com>
2023-07-09 18:27:37 -0400

    agregando la base de datos y archivos axios y materialize

--- vue3/megadron/src/main.js
+++ vue3/megadron/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/css/materialize.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.min.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios,axios).mount('#app')

e533c029c6aa30b76875ae3d40348b9fdcdc4fe3
raul cervantes <raulcer.rmc@gmail.com>
2023-07-09 19:52:03 -0400

    Agregando componente de Menu con el nombre Menu.vue  y componente de pie de pagina Footer.vue

--- vue3/megadron/src/App.vue
+++ vue3/megadron/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Menu></Menu>    
+  <router-view/> 
+  <FootN></FootN>
 </template>
 
 <style lang="scss">
@@ -14,47 +12,22 @@
   text-align: center;
   color: #2c3e50;
 }
-.color-box {
-                width: 40px;
-                height: 40px;
-                border-radius: 50%;
-                margin: 7px;
-                display: inline-block;
-            }
-
-            .clic{
-                cursor: pointer;
-            }
-
-            .quantity button{
-                border-radius: 50%;
-                display: inline-block;
-                width: 30px;
-            }
-            .quantity div{
-                text-align: center;
-                min-width: 30px;
-                display: inline-block;
-                font-weight: bold;
-            }
-            .buy-box{
-                margin: 20px;
-            }
-            footer {
-                
-                text-align: center;
-                padding: 30px 10px;
-                margin-top: 50px;
-                min-height: 100px;
-            }
-            .container{
-                margin-top: 50px;
-            }
-            .producto-relacionado-precio{
-                background: orangered;
-                color: white;
-                text-align: center;
-                padding: 10px; 
-            }
 
 </style>
+<script>
+import Menu from '@/components/Menu.vue';
+import FootN from '@/components/Footer.vue';
+export default {
+  name: 'App',
+  data(){
+    return {
+    }
+  },
+  methods: {
+    
+  },
+  components:{
+    Menu,FootN
+  }
+}
+</script>
---
+++ vue3/megadron/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <footer :style="'background-color:'+footerColor">    
+        </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FootN',
+    props: {
+        
+    },
+    data(){
+       return {
+        footerColor: "slategrey",
+       }
+    },
+    created() {
+        
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+ footer {
+                
+                text-align: center;
+                padding: 30px 10px;
+                margin-top: 50px;
+                min-height: 100px;
+            }
+  </style>
